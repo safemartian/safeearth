032e7221034d1e59405b304898dc54e5d0cb5859
safemartian <81262808+safemartian@users.noreply.github.com>
2021-05-08 12:05:11 +0200

    Removed code that triggers the modal on load

--- assets/js/main.js
+++ assets/js/main.js
@@ -280,27 +280,27 @@
 		
 } ) ( jQuery );
 
-setTimeout ( () => {
-		var modal = document.getElementById ( "myModal" );
+// setTimeout ( () => {
+// 		var modal = document.getElementById ( "myModal" );
 		
-		// Get the <span> element that closes the modal
-		var span = document.getElementById ( 'close-modal' );
+// 		// Get the <span> element that closes the modal
+// 		var span = document.getElementById ( 'close-modal' );
 		
-		modal.style.display = "flex";
+// 		modal.style.display = "flex";
 		
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = () => {
-				console.error ( 'muie' );
-				console.error ( modal );
-				modal.style.display = "none";
-		};
+// 		// When the user clicks on <span> (x), close the modal
+// 		span.onclick = () => {
+// 				console.error ( 'muie' );
+// 				console.error ( modal );
+// 				modal.style.display = "none";
+// 		};
 		
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function ( event ) {
-				if ( event.target == modal ) {
-						modal.style.display = "none";
-				}
-		};
-}, 3000 );
+// 		// When the user clicks anywhere outside of the modal, close it
+// 		window.onclick = function ( event ) {
+// 				if ( event.target == modal ) {
+// 						modal.style.display = "none";
+// 				}
+// 		};
+// }, 3000 );
 
 
